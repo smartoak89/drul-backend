7e4d83f7fc28d88b20b5e9676c1c72264a07ec3d
yura <smartoak89@gmail.com>
2017-03-20 16:30:41 +0200

    fix cross header

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(require('./middleware/sendHttpError'));
 app.use(require('./middleware/sendMessage'));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     next();
 });
 
